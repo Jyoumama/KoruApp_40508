0c9508de66ea4db5b1a7c649f7ec576f030125bd
Jyoumama <juri19771211@gmail.com>
2024-05-06 10:47:34 +0900

    マイページを作成し予約を削除する実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "users/show"
   root to: 'reservations#index'
   resources :reservations do
     resources:orders, only:[:index,:create]
   end
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show ]
 end
