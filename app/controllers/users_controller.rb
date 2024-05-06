24163e031f06e0b322e71482d059e3d270614b99
Jyoumama <juri19771211@gmail.com>
2024-05-06 23:48:23 +0900

    ルーティングの修正とコントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
     current_user.update(user_params)
   end
 
+  def show
+    @user = User.find(current_user.id)
+    @user_reservations = current_user.reservations.where("start_time >= ?", DateTime.current).order(day: :desc)
+    @visit_historys = current_user.reservations.where("start_time < ?", DateTime.current).where("start_time > ?", DateTime.current << 12).order(day: :desc)
+  end
+
   def destroy
   end
 
