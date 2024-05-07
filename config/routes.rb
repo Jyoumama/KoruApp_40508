365b93aa0721584674b448c27a75db81e6f86e72
Jyoumama <juri19771211@gmail.com>
2024-05-08 00:16:02 +0900

    ルーティングの再修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'reservations#index'
-  resources :orders, only: [:index, :create, :show]
-  resources :reservations, only: [:show, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:edit, :update, :show ]
+  resources :reservations do
+    resources :orders, only: [:index, :create,:show]
+  end
+  resources :users, only: [:edit, :update, :show]
 end
