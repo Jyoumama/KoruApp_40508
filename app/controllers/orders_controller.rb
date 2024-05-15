23ea4b7e6aba109b790f0bee1bd34e29ef3bc5e7
Jyoumama <juri19771211@gmail.com>
2024-05-15 22:41:15 +0900

    環境変数に変換

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   def index
     @reservation = Reservation.find(params[:reservation_id])
+    @payjp_public_key = ENV['PAYJP_PUBLIC_KEY']
   end
 
   def create
