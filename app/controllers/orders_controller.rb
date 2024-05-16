a0f4c092fdc7c762682d14f74f0fe6c3cc334a18
Jyoumama <juri19771211@gmail.com>
2024-05-16 10:38:43 +0900

    環境変数の設定（S鍵）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,21 @@ class OrdersController < ApplicationController
   end
 
   def create
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    token = params[:payjpToken]
+
+    charge = Payjp::Charge.create(
+      amount: 1000, # ここに決済金額を入れてね
+      card: token,
+      currency: 'jpy'
+    )
+
+    if charge.paid
+      flash[:notice] = "決済が完了しました！"
+      redirect_to root_path
+    else
+      flash[:alert] = "決済に失敗しました..."
+      render :index
+    end
   end
 end
